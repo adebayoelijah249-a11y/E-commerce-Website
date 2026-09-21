60df36fff06390092e7d8c0ffdc6d050e7f30c31
adebayoelijah249-a11y <adebayoelijah249@gmail.com>
2026-09-21 11:00:22 +0100

    first commit

---
+++ src/product.js
@@ -0,0 +1,98 @@
+const productList = [
+  {
+    id: 1,
+    name: "T-shirt with Tape Details",
+    img: "./assets/image 7-Photoroom.png",
+    price: 120,
+    description: "High-performance laptop with 16GB RAM and 512GB SSD.",
+  },
+  {
+    id: 2,
+    name: "Skinny fit Jeans",
+    img: "./assets/image 8-Photoroom.png",
+    price: 240,
+    description:
+      "Latest model smartphone with advanced camera and long battery life.",
+  },
+  {
+    id: 3,
+    name: "Checkered Shirt",
+    img: "./assets/image 9-Photoroom.png",
+    price: 180,
+    description:
+      "Premium noise-canceling headphones with superior sound quality.",
+  },
+  {
+    id: 4,
+    name: "Sleeved Striped Shirt",
+    img: "./assets/image 10-Photoroom.png",
+    price: 130,
+    description: "Versatile tablet perfect for work and entertainment.",
+  },
+];
+
+const topList = [
+  {
+    id: 5,
+    name: "Vertical Striped Shirt",
+    img: "./assets/image 7 (1)-Photoroom.png",
+    price: 212,
+    description: "High-performance laptop with 16GB RAM and 512GB SSD.",
+  },
+  {
+    id: 6,
+    name: "Courage Graphic T-shirt",
+    img: "./assets/image 8 (1)-Photoroom.png",
+    price: 145,
+    description:
+      "Latest model smartphone with advanced camera and long battery life.",
+  },
+  {
+    id: 7,
+    name: "Loose Fit Bermuda Short",
+    img: "./assets/image 9 (1)-Photoroom.png",
+    price: 80,
+    description:
+      "Premium noise-canceling headphones with superior sound quality.",
+  },
+  {
+    id: 8,
+    name: "Faded Skinny Jeans",
+    img: "./assets/image 10 (1)-Photoroom.png",
+    price: 210,
+    description: "Versatile tablet perfect for work and entertainment.",
+  },
+];
+
+const productListTwo = [
+  {
+    id: 1,
+    name: "Polo with Contrast Trims",
+    img: "./assets/checkered-shirt.png",
+    price: 212,
+    description: "High-performance laptop with 16GB RAM and 512GB SSD.",
+  },
+  {
+    id: 2,
+    name: "Gradient Graphic T-shirt",
+    img: "./assets/Frame 33.png",
+    price: 240,
+    description:
+      "Latest model smartphone with advanced camera and long battery life.",
+  },
+  {
+    id: 3,
+    name: "Polo with Tipping Details",
+    img: "./assets/Frame 34.png",
+    price: 180,
+    description:
+      "Premium noise-canceling headphones with superior sound quality.",
+  },
+  {
+    id: 4,
+    name: "Black Stripped T-shirt",
+    img: "./assets/Frame 38.png",
+    price: 120,
+    description: "Versatile tablet perfect for work and entertainment.",
+  },
+];
---
+++ src/productdetail.js
@@ -0,0 +1,32 @@
+const productList = [
+  {
+    id: 1,
+    name: "Polo with Contrast Trims",
+    img: "./assets/Frame 32.png",
+    price: 212,
+    description: "High-performance laptop with 16GB RAM and 512GB SSD.",
+  },
+  {
+    id: 2,
+    name: "Gradient Graphic T-shirt",
+    img: "./assets/Frame 33.png",
+    price: 240,
+    description:
+      "Latest model smartphone with advanced camera and long battery life.",
+  },
+  {
+    id: 3,
+    name: "Polo with Tipping Details",
+    img: "./assets/Frame 34.png",
+    price: 180,
+    description:
+      "Premium noise-canceling headphones with superior sound quality.",
+  },
+  {
+    id: 4,
+    name: "Black Stripped T-shirt",
+    img: "./assets/Frame 38.png",
+    price: 120,
+    description: "Versatile tablet perfect for work and entertainment.",
+  },
+];
